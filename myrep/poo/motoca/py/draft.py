f9ae98a35c66262e0614afa2ef6ed1e73ed13872
Layanne <layannemenezes78@gmail.com>
2025-10-27 16:53:16 -0300

    motodesgracada

--- myrep/poo/motoca/py/draft.py
+++ myrep/poo/motoca/py/draft.py
@@ -1,55 +1,69 @@
-class Moto:
-    def __init__(self,pot:int=1, tempo:int=0, pessoa:int=0):
-        self.__pot=1
-        self.__tempo=0
-        self.__pessoa="(empty)"
-        self.setPot(pot)
-        self.setTempo(tempo)
-        self.setPessoa(pessoa)
+class Pessoa:
+    def __init__(self, name:str, age:int):
+        self.__age=age
+        self.__name=name
 
-    def getPot(self):
-        return self.__pot
-    def getTempo(self):
-        return self.__tempo
-    def getPessoa(self):
-        return self.__pessoa
+    def getAge(self):
+        return self.__age
     
-    def setPot(self, pot:int):
-        if pot>0:
-            self.__pot=pot
-        else:
-            print("fail: potencia invalida")
-        
-    def setTempo(self, tempo:int):
-        if tempo>=0:
-                self.__tempo=tempo
-        else:
-                print("fail: tempo invalido")
+    def getname(self):
+        return self.__name
 
-    def setPessoa(self, pessoa:int):
-        if pessoa == 0:
-            self.__pessoa = "(empty)"
-        elif 1 <= pessoa <= 2:
-            self.__pessoa = pessoa
-        else:
-            print("fail: numero de pessoas invalido")
+    def __str__(self):
+        return f"{self.__name}:{self.__age}"
 
-    def entrar(self):
-        if self.__pessoa!="(empty)":
-            print("fail: moto ocupada")
+class Moto:
+    def __init__(self, pot: int = 1, tempo: int = 0):
+        self.__pot = pot
+        self.__tempo = tempo
+        self.__pessoa = None 
+
+    def getPessoa(self):
+        if self.__pessoa is None:
+            return "(empty)"
+        return f"({self.__pessoa})"
+
+    def entrar(self, name: str, age: int):
+        if self.__pessoa is not None:
+            print("fail: busy motorcycle")
         else:
-            self.__pessoa=1
+            self.__pessoa = Pessoa(name, age) 
 
     def sair(self):
-        if self.__pessoa=="(empty)":
-            print("fail: moto vazia")
+        if self.__pessoa is None:
+            print("fail: empty motorcycle")
+            return None
         else:
-            self.__pessoa="(empty)"
-
-    def comprar(self):
+            pessoa_saiu = self.__pessoa
+            self.__pessoa = None
+            print(pessoa_saiu)  
+            return pessoa_saiu
         
-    def __str__(self)->str:
-        return f"power:{self.__pot}, time:{self.__tempo}, person:{self.__pessoa}"
+    def comprar(self, tempo: int):
+        self.__tempo += tempo
+
+    def honk(self):
+        print("P" + "e" * self.__pot + "m")
+
+    def drive(self,minutos:int):
+        if self.__tempo<=0:
+            print("fail: buy time first")
+            return
+        if self.__pessoa is None:
+            print("fail: empty motorcycle")
+            return
+        
+        if self.__pessoa.getAge()>10:
+            print("fail: too old to drive")
+            return
+        if minutos>self.__tempo:
+            print(f"fail: time finished after {self.__tempo} minutes")
+            self.__tempo=0
+        else:
+            self.__tempo-=minutos
+
+    def __str__(self):
+        return f"power:{self.__pot}, time:{self.__tempo}, person:{self.getPessoa()}"
 
 def main():
     moto=Moto()
@@ -63,10 +77,15 @@ def main():
         if args[0]=="show":
             print(moto)
         if args[0]=="init":
-            moto=Moto(int(args[1]), int(args[2]), int(args[3]))
+            moto = Moto(int(args[1]))
         if args[0]=="enter":
-            moto.entrar()
+            moto.entrar(str(args[1]), int(args[2]))
         if args[0]=="leave":
             moto.sair()
-
+        if args[0]=="buy":
+            moto.comprar(int(args[1]))
+        if args[0]=="drive":
+            moto.drive(int(args[1]))
+        if args[0]=="honk":
+            moto.honk()
 main()
